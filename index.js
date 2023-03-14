3addd6d3c1a824c7f74a62c4b4f6330494937fd1
Aleksandr Savchenko <titamoto@gmail.com>
2023-03-14 12:21:05 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 62;
+let num2 = 1;
 
-// Write your code here
+let num3 = 9;
+let num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max (2, 10, 20, 5, 7);
+
+
+
